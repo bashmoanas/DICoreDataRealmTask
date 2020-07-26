8efcf0648544e14f633caf9b05b08114e6f6034f
Anas Bashandy <anasmbash@icloud.com>
2020-07-26 12:59:30 +0200

    Initial Commit

---
+++ DICoreDataRealmTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DICoreDataRealmTask
+//
+//  Created by Anas Bashandy on 26/7/20.
+//  Copyright © 2020 Anas Bashandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
